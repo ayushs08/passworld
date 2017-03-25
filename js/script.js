ba7916a72b555ffe616deb555a8e7f46b503abab
Rishabh Nambiar <rishabhn@protonmail.com>
2017-03-25 17:37:34 +0530

    Added wordlist.

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+var flavourDict1 = ["path", "rabbit", "dress", "page", "replied", "bus", "product", "sky", "wonder" ]; 
+var flavourDict2 = ["river", "river", "chapter", "respect", "medicine", "outside", "available", "replace", "life" ]; 
+var flavourDict3 = ["car", "orbit", "region", "wing", "section", "short", "money", "suppose", "lonely" ]; 
+var flavourDict4 = ["swim", "substance", "fill", "black", "use", "pet", "high", "strong", "national" ]; 
+var flavourDict5 = ["until", "second", "motion", "hair", "purple", "charge", "represent", "enjoy", "more" ]; 
+var flavourDict6 = ["single", "shall", "musical", "dirt", "speech", "gentle", "plastic", "affect", "break" ]; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
